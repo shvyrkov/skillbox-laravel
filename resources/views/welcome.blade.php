163a62e06cb83c555bdc20f631b422d97c3eaf44
Shvyrkov Yuri <mailto:shvyrkov@yandex.ru>
2022-12-12 11:09:34 +0300

    Revert "Git checkout commit no file in command"
    This reverts commit 9d4fb211e8e94ae259f369df983b42cf94d6e793.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
             <div class="row py-lg-5">
                 <div class="col-lg-6 col-md-8 mx-auto">
                     <h1 class="fw-light">Album example</h1>
-                    <h2>Git checkout commit test</h2>
+                    <h1 class="fw-light">Simple test on branch test-2</h1>
                     <h2 class="fw-light">Git diff test-4</h2>
                     <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
                     <p>
